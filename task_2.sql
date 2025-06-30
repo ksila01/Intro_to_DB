833c5e6eac39e27bef415049bf330537312dd156
Sila <ksila04@gmail.com>
2025-06-30 19:32:32 +0300

    task_2

--- task_2.sql
+++ task_2.sql
@@ -1,13 +1,17 @@
+-- Create the database if it doesn't already exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the alx_book_store database
 USE alx_book_store;
 
 -- Create the Authors table
-CREATE TABLE authors (
+CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Create the Books table
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -17,7 +21,7 @@ CREATE TABLE books (
 );
 
 -- Create the Customers table
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -25,19 +29,19 @@ CREATE TABLE customers (
 );
 
 -- Create the Orders table
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) -- Foreign Key for customer_id
 );
 
 -- Create the Order Details table
-CREATE TABLE order_details (
+CREATE TABLE IF NOT EXISTS order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id), -- Foreign Key for order_id
+    FOREIGN KEY (book_id) REFERENCES books(book_id) -- Foreign Key for book_id
 );
